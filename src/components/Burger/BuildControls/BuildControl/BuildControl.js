cd9fef0078b923bc1ad0e9f81e56cf954013a2fd
Danones <diogo.sousa.uk@gmail.com>
2020-12-12 15:08:31 +0000

    button ADD and Remove working now with safety logic

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,11 +2,21 @@ import React from 'react';
 import classes from './BuildControl.css';
 
 const buildControl = (props) => {
-   <div className={classes.BuildControl}>
-      <div className={classes.label}>{props.label}</div>
-      <button classeName={classes.Less}>Less</button>
-      <button className={classes.More}>More</button>
-   </div>;
+   return (
+      <div className={classes.BuildControl}>
+         <div className={classes.Label}>{props.label}</div>
+         <button
+            className={classes.Less}
+            onClick={props.removed}
+            disabled={props.disabled}
+         >
+            Less
+         </button>
+         <button className={classes.More} onClick={props.added}>
+            More
+         </button>
+      </div>
+   );
 };
 
 export default buildControl;
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -14,7 +14,15 @@ const buildControls = (props) => {
    return (
       <div className={classes.BuildControls}>
          {controls.map((ctrl) => {
-            <BuildControl key={ctrl.label} label={ctrl.label} />;
+            return (
+               <BuildControl
+                  key={ctrl.label}
+                  label={ctrl.label}
+                  added={() => props.ingredientAdded(ctrl.type)}
+                  removed={() => props.ingredientRemoved(ctrl.type)}
+                  disabled={props.disabled[ctrl.type]}
+               />
+            );
          })}
       </div>
    );
