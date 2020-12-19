067a50e8b1e30990b7a2f8eb20b73d55f0f3ca43
Danones <diogo.sousa.uk@gmail.com>
2020-12-19 20:17:11 +0000

    Firebase and Axios added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders.js';
 
 const INGREDIENT_PRICES = {
    salad: 0.5,
@@ -84,7 +85,25 @@ class BurgerBuilder extends Component {
    };
 
    purchaseContinueHandler = () => {
-      alert('Give me your money!!');
+      // alert('Give me your money!!');
+      const order = {
+         ingredients: this.state.ingredients,
+         price: this.state.totalPrice,
+         customer: {
+            name: 'Diogo',
+            adress: {
+               street: 'teststreet',
+               zipCode: '4000',
+               country: 'Portugal',
+            },
+            email: 'diogo@gmail.com',
+         },
+         deliveryMethod: 'fastest',
+      };
+      axios
+         .post('/orders.json', order)
+         .then((response) => console.log(response))
+         .catch((error) => console.log(error));
    };
 
    render() {
