221f6363beeeba6afe7b88e66e29da6f83c38c9c
Danones <diogo.sousa.uk@gmail.com>
2020-12-31 12:01:54 +0000

    Added contactData component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
    state = {
@@ -11,22 +13,36 @@ class Checkout extends Component {
       },
    };
 
-   checkoutCancelledHandler = () =>{
-      this.props.history.goBack()
+   componentDidMount() {
+      const query = new URLSearchParams(this.props.location.search);
+      const ingredients = {};
+      for (let param of query.entries()) {
+         // ['salad', '1']
+         ingredients[param[0]] = +param[1];
+      }
+      this.setState({ ingredients: ingredients });
    }
 
+   checkoutCancelledHandler = () => {
+      this.props.history.goBack();
+   };
 
-   checkoutContinueHandler = () => {
-      this.props.history.replace('/checkout/contact-data')
-   }
+   checkoutContinuedHandler = () => {
+      this.props.history.replace('/checkout/contact-data');
+   };
 
    render() {
       return (
          <div>
-            <CheckoutSummary 
+            <CheckoutSummary
+               ingredients={this.state.ingredients}
                checkoutCancelled={this.checkoutCancelledHandler}
-               checkoutContinued={this.checkoutContinueHandler}
-               ingredients={this.state.ingredients} />
+               checkoutContinued={this.checkoutContinuedHandler}
+            />
+            <Route
+               path={this.props.match.path + '/contact-data'}
+               component={ContactData}
+            />
          </div>
       );
    }
