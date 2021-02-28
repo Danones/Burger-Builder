78e572d15433230b6e05d6460a51a782e253454c
Danones <diogo.sousa.uk@gmail.com>
2021-02-28 11:09:56 +0000

    Do Something

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
                <Route path="/checkout" component={Checkout} />
                <Route path="/orders" component={Orders} />
                <Route path="/" exact component={BurgerBuilder} />
+
             </Layout>
          </div>
       );
